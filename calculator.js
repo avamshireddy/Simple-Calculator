7e03a0ad1411c152b134d4d6ce6fe3e1c0d5b9c8
vamshi krishana reddy <138650632+avamshireddy@users.noreply.github.com>
2024-06-17 23:41:05 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,23 @@
+// CALCULATOR PROGRAM
+
+const display=document.getElementById("display");
+
+function appendToDisplay(input){
+display.value+=input;
+}
+function clearDisplay(){
+display.value="";
+}
+function calculate(){
+display.value=eval(display.value);
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="Error";
+    }
+        
+}
